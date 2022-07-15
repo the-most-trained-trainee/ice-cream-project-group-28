56728c43c4614c09aba7ca4895e439eb524bb64e
Maksym <maksstair@gmail.com>
2022-07-15 17:46:36 +0300

    add new sprite

--- src/mob-menu.js
+++ src/mob-menu.js
@@ -1,16 +1,16 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector(".mob-menu__open-btn"),
-      closeMenuBtn: document.querySelector(".mob-menu__close-btn"),
-      menu: document.querySelector(".backdrop-mob"),
-      body: document.querySelector("body"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
-      refs.body.classList.toggle("no-scroll");
-    }
-  })();
+  const refs = {
+    openMenuBtn: document.querySelector(".mob-menu__open-btn"),
+    closeMenuBtn: document.querySelector(".mob-menu__close-btn"),
+    menu: document.querySelector(".backdrop-mob"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();

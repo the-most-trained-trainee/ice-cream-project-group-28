1817f6f298d9c18f01a62b9941f42fb8a450b6b3
Anton <a.chertok@akatech.com.ua>
2022-07-19 12:52:10 +0300

    update

--- src/js/products-overlay.js
+++ src/js/products-overlay.js
@@ -1,3 +1,26 @@
-const overlayButton = document.querySelectorAll('[js-overlay-button]');
+const buttonsStart = document.querySelectorAll('[js-overlay-start]');
+const buttonsReturn = document.querySelectorAll('[js-overlay-return]');
 
-overlayButton.forEach
+buttonsStart.forEach(button => { 
+    button.addEventListener('click', overlayStart);
+});
+
+function overlayStart() {
+    console.log('overlay starts');
+}
+
+
+
+// products.forEach(product => {
+//     product.onmouseover = () => {
+//         // button.classList.toggle('animate__bounce');
+//         button.classList.add('animate__animated', 'animate__swing');
+//         // button.classList.add('animate__swing');
+//     };
+
+//     product.onmouseout = () => {
+//         button.classList.remove('animate__animated', 'animate__swing');
+//         // button.classList.remove('animate__swing');
+//     };
+
+// });

0b5e9c169d9bb4af0aa5c33d710dd73ee2b7b9f9
Anton <a.chertok@akatech.com.ua>
2022-07-19 18:05:15 +0300

    update overlay

--- src/js/products-overlay.js
+++ src/js/products-overlay.js
@@ -1,39 +1,64 @@
 const startButtons = document.querySelectorAll('[js-overlay-start]');
-const returnButtons = document.querySelectorAll('[js-overlay-return]');
 const cards = document.querySelectorAll('.card__wrapper');
 
-cards.forEach(card => {
-    
+cards.forEach(card => {    
     let cardBgColor = getComputedStyle(card).backgroundColor ;
     let overlay = card.querySelector('.products__overlay');
     overlay.style.backgroundColor = cardBgColor;
 });
 
-startButtons.forEach(button => { 
-    button.addEventListener('click', function (event) { 
+
+startButtons.forEach(button => {
+    button.addEventListener('click', function (event) {
         event.preventDefault;
-        let target = event.target;
-        
-        // console.log(target.closest('.card__wrapper').querySelector('.products__overlay'));
-        
+        let target = event.target;    
         let overlay = target.closest('.card__wrapper').querySelector('.products__overlay');
         
-        overlay.classList.add('products__overlay--start');
+        overlay.classList.toggle('products__overlay--start');
 
+        this.classList.toggle('return-arrow');
     });
 });
 
-returnButtons.forEach(button => { 
-    button.addEventListener('click', function (event) { 
-        event.preventDefault;
-        let target = event.target;
-        
-        // console.log(target.closest('.card__wrapper').querySelector('.products__overlay'));
-        let overlay = target.closest('.products__overlay');
-        
-        overlay.classList.remove('products__overlay--start');
 
-    });
-});
+
+
+// const startButtons = document.querySelectorAll('[js-overlay-start]');
+// const returnButtons = document.querySelectorAll('[js-overlay-return]');
+// const cards = document.querySelectorAll('.card__wrapper');
+
+// cards.forEach(card => {
+    
+//     let cardBgColor = getComputedStyle(card).backgroundColor ;
+//     let overlay = card.querySelector('.products__overlay');
+//     overlay.style.backgroundColor = cardBgColor;
+// });
+
+// startButtons.forEach(button => { 
+//     button.addEventListener('click', function (event) { 
+//         event.preventDefault;
+//         let target = event.target;
+        
+//         // console.log(target.closest('.card__wrapper').querySelector('.products__overlay'));
+        
+//         let overlay = target.closest('.card__wrapper').querySelector('.products__overlay');
+        
+//         overlay.classList.add('products__overlay--start');
+
+//     });
+// });
+
+// returnButtons.forEach(button => { 
+//     button.addEventListener('click', function (event) { 
+//         event.preventDefault;
+//         let target = event.target;
+        
+//         // console.log(target.closest('.card__wrapper').querySelector('.products__overlay'));
+//         let overlay = target.closest('.products__overlay');
+        
+//         overlay.classList.remove('products__overlay--start');
+
+//     });
+// });
 
 

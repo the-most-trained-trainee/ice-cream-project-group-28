87438c74831d10a8da5a1e984ba1b0fc9bd13b6a
Maksym <maksstair@gmail.com>
2022-07-19 20:47:52 +0300

    finish-fixing

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -26,6 +26,5 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
